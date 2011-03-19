23efad5fa022a393931b0d81f8f31eb7bcf47499
Fernando Noronha <fenoronha@gmail.com>
2011-03-19 13:12:56 -0300

    limpeza de código

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<?php
-//defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') or die('No direct script access.');
 ini_set('display_errors', 1);
 
 define('EXT', '.php');
